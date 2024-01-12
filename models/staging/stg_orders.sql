f7aa1e3375869cc6c3ee6087fbc21eaec7ed48ef
Pawan Chahar <pawan.chahar.ey@gmail.com>
2024-01-12 23:09:43 +0000

    added macros

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -21,10 +21,14 @@ c.state ,
 p.productid,
 p.category ,
 p.subcategory ,
-p.productname
+p.productname ,
+{{ markup('ordersellingprice' ,'ordercostprice') }} as markup
 
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p
 on o.productid = p.productid
+
+
+{{limit_data_in_dev('orderdate')}}
